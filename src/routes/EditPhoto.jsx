14f5b59a47c716dec6f0c21d15f6280f767b4a59
aryaputra <aryaputrawibawa06@gmail.com>
2022-12-15 15:50:16 +0700

    commit tiga

--- src/routes/EditPhoto.jsx
+++ src/routes/EditPhoto.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -44,7 +45,6 @@ const EditPhoto = () => {
     setLoading(true);
     getdata()
       setLoading(false)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   if (error) return <div>Error!</div>;
